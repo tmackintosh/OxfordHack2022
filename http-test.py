d5f372fd422d3395a761bfc923f554d5f1bc9662
Tom Mackintosh <tom.mackintosh2001@live.co.uk>
2022-02-26 15:32:53 +0000

    Get business information

--- http-test.py
+++ http-test.py
@@ -1,6 +1,13 @@
 import requests
+import json
 
-def get_search_results():
-    r = requests.get("https://customsearch.googleapis.com/customsearch/v1?cx=c8129af6c4906773b&q=facebook&key=AIzaSyDsRiDvlwLUNwqaExJoivtP_xE1_BicHio")
-    data = r.json()
-    print(data)
+ENGINE_ID = "c8129af6c4906773b"
+API_KEY = "AIzaSyDsRiDvlwLUNwqaExJoivtP_xE1_BicHio"
+
+def get_search_results(query):
+    r = requests.get("https://customsearch.googleapis.com/customsearch/v1?cx=" + ENGINE_ID + "&q=" + query + "&key=" + API_KEY)
+    return r.json()
+
+def get_place_by_name(name):
+    r = requests.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + name + "&key=" + API_KEY)
+    return r.json()
