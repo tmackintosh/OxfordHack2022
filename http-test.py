fd1b3a206a1b040d0c616cc6f0c82e55cc33d0f2
Tom Mackintosh <tom.mackintosh2001@live.co.uk>
2022-02-27 07:07:42 +0000

    Get distance from city centre

--- http-test.py
+++ http-test.py
@@ -22,6 +22,28 @@ def get_place_reviews(place_details):
     except:
         return ["No reviews available."]
 
+def get_lat_lng_from_place(place):
+    geometry = place["geometry"]
+    location = geometry["location"]
+    return location["lat"], location["lng"]
+
+def get_city_name(place):
+    try:
+        compound_code = place["plus_code"]["compound_code"]
+        first_space = compound_code.find(" ")
+    except KeyError:
+        compound_code = place["formatted_address"]
+        first_space = compound_code.find(",")
+
+    return compound_code[first_space + 1:]
+
+def get_distance_from_centre(place, city):
+    place_lat, place_lng = get_lat_lng_from_place(place)
+    city_lat, city_lng = get_lat_lng_from_place(city)
+
+    # Pythagoras' Theorem
+    return ((place_lat - city_lat) ** 2) + ((place_lng - city_lng) ** 2)
+
 places = get_place_by_name(input("Enter place: "))
 
 for place in places["results"]:
@@ -29,8 +51,7 @@ for place in places["results"]:
     place_details = get_place_details(place_id)
     place_reviews = get_place_reviews(place_details)
 
-    for review in place_reviews:
-        try:
-            print(review["text"])
-        except:
-            continue
+    city_name = get_city_name(place)
+    city = get_place_by_name(city_name)["results"][0]
+
+    distance_from_centre = get_distance_from_centre(place, city)
